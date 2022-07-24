c1a67bea90900a447914f0bcb795b2a64049ebcf
Hakan ÖZER <hakanozer02@gmail.com>
2022-07-24 16:42:55 +0300

    Slf4j

--- 7-Days/src/main/java/com/example/config/GlobalException.java
+++ 7-Days/src/main/java/com/example/config/GlobalException.java
@@ -1,5 +1,7 @@
 package com.example.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +14,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.*;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalException extends ResponseEntityExceptionHandler {
+
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Map<String, Object> hm = new LinkedHashMap<>();
+        List<Map> ls = errorsParse(ex);
         hm.put("status", false);
-        hm.put("errors", errorsParse(ex) );
+        hm.put("errors", ls );
+        ls.forEach( item -> {
+            log.error( item.toString() );
+        } );
         return new ResponseEntity(hm, HttpStatus.BAD_REQUEST);
     }
 
